a43b64d704509a853bbde4d6c757edce444cebbd
Yann Kurzo <ya_246@hotmail.com>
2015-06-03 00:27:10 +0200

    Update Makefile configuration

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -62,11 +62,10 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/calculator
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk calculator
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/calculator: ${OBJECTFILES}
-	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/calculator ${OBJECTFILES} ${LDLIBSOPTIONS}
+calculator: ${OBJECTFILES}
+	${LINK.cc} -o calculator ${OBJECTFILES} ${LDLIBSOPTIONS}
 
 ${OBJECTDIR}/src/analyse.o: src/analyse.cc 
 	${MKDIR} -p ${OBJECTDIR}/src
@@ -109,7 +108,7 @@ ${OBJECTDIR}/src/token.o: src/token.cc
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/calculator
+	${RM} calculator
 
 # Subprojects
 .clean-subprojects:
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -8,9 +8,9 @@ CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=
 CND_ARTIFACT_NAME_Debug=calculator
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/calculator
+CND_ARTIFACT_PATH_Debug=calculator
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=calculator.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/calculator.tar
