be0264d80123d3e1eec1ef9e985aff7879aed9e3
Yann Kurzo <yann.kurzo@hefr.ch>
2015-06-11 17:14:04 +0200

    Add lexer file and modify configure

---
+++ src/include/lexer.h
@@ -0,0 +1,22 @@
+
+//  ==========================================================================
+/// @file   lexer.h
+/// @author Yann Kurzo
+/// @date   June 11, 2015, 2:06 PM
+/// @license GPL2
+/// @brief  
+//  ==========================================================================
+
+#ifndef LEXER_H
+#define	LEXER_H
+
+/// @brief  This class simulates a binary tree.
+///         
+class Lexer
+{
+	
+};
+
+
+#endif	/* LEXER_H */
+
