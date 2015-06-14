7c8cbbcad1cd55d9ef4ab658f9aec02058234fea
Yann Kurzo <ya_246@hotmail.com>
2015-06-14 23:29:35 +0200

    Update comments

--- src/command.cc
+++ src/command.cc
@@ -4,19 +4,18 @@
 /// @author Yann Kurzo
 /// @date   June 14, 2015, 9:32 PM
 /// @license GPL2
-/// @brief  
+/// @brief  Class to handle the different commands and arguments
 //  ==========================================================================
 
 #include "command.h"
 
 using namespace std;
 
-
 Command::command_t Command::getArgument(std::string str)
 {
     for(int i=0; i<eNB_COMMANDS; ++i)
     {
-        if(str == ("--" + commands[i]) || str == argumentsShortcut[i])
+        if(str == ("--" + commands[i]) || str == commandsShortcut[i])
         {
             return static_cast<command_t>(i);
         }
@@ -28,7 +27,7 @@ Command::command_t Command::getCommand(std::string str)
 {
     for(int i=0; i<eNB_COMMANDS; ++i)
     {
-        if(str == commands[i] || str == argumentsShortcut[i])
+        if(str == commands[i] || str == commandsShortcut[i])
         {
             return static_cast<command_t>(i);
         }
--- src/include/command.h
+++ src/include/command.h
@@ -4,7 +4,7 @@
 /// @author Yann Kurzo
 /// @date   June 14, 2015, 9:32 PM
 /// @license GPL2
-/// @brief  
+/// @brief  Class to handle the different commands and arguments
 //  ==========================================================================
 
 #ifndef COMMAND_H
@@ -13,23 +13,23 @@
 #include <string>
 
 
-
-static const std::string argumentsShortcut[] =
-{
-    "-l"
-};
-
+/// @brief  List of commands
 static const std::string commands[] =
 {
     "list-available-functions"
 };
 
+/// @brief  List of commands shortcut
+static const std::string commandsShortcut[] =
+{
+    "-l"
+};
 
-/// @brief  
+/// @brief  Class to handle the different commands and arguments
 class Command
 { 
     public:
-        /// @brief
+        /// @brief  List of available commands and arguments
         typedef enum
         {
             eARGUMENT_LIST_AVAILABLE_FUNCTIONS,
@@ -41,15 +41,13 @@ class Command
         std::string str_m;
 
     public:
-        /// @brief  
+        /// @brief  Get the actual argument
+        /// @return Argument, or eNB_COMMANDS elsewhere
         static command_t getArgument(std::string str);
         
-        /// @brief  
+        /// @brief  Get the actual command
+        /// @return Command, or eNB_COMMANDS elsewhere
         static command_t getCommand(std::string str);
-        
-    private:
-        
-        /// @brief  
 };
 
 #endif	/* COMMAND_H */
